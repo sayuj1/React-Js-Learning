225312be2a7eefe290b816bff141b32ae7530c50
Sayuj Sehgal <sayujsehgal@iprakash.com>
2020-02-28 15:52:05 +0530

    react fragment added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 // let el = < h1 > Hello World1! < /h1>   
 
 // export default el;
 
-class App extends Component {
-    render() {
-        // return React.createElement("h1", null, "Hello World2!");
-        return <h1> Hello World3! </h1>;
-    }
-}
+// class App extends Component {
+//     render() {
+//         // return React.createElement("h1", null, "Hello World2!");
+//         return <h1> Hello World3! </h1>;
+//     }
+// }
 
 //App-- > Component  
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>Hello World1</h1>
+                <h2>Hellow World2</h2>
+            </Fragment>
+        );
+    }
+}
 export default App;
