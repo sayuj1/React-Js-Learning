43a439b05052a8e138059867b06ed533621d41f7
Sayuj Sehgal <sayujsehgal@iprakash.com>
2020-03-02 20:59:03 +0530

    useState hook

---
+++ src/Basics of React js/App.js
@@ -0,0 +1,65 @@
+import React, { Component, Fragment } from "react";
+// import Student from "./Student";
+
+// let el = < h1 > Hello World1! < /h1>   
+
+// export default el;
+
+// class App extends Component {
+//     render() {
+//         // return React.createElement("h1", null, "Hello World2!");
+//         return <h1> Hello World3! </h1>;
+//     }
+// }
+
+//App-- > Component  
+
+// class App extends Component {
+//     render() {
+//         return (
+//             <Fragment>
+//                 <h1>Hello World1</h1>
+//                 <h2>Hellow World2</h2>
+//             </Fragment>
+//         );
+//     }
+// }
+
+// export default App;
+
+// const el = <h1> Hello Sayuj </h1>;
+// export default el;
+
+// const Student = (props) => {
+//     return <h1>Hello {props.name}</h1>
+// }
+
+// class Student extends Component {
+//     render() {
+//         return <h1> Hello {this.props.name} </h1>
+//     }
+// }
+
+// export default Student;
+
+// const App = () => {
+//     return (
+//         <Fragment>
+//             <Student name="sayuj" />
+//             <Student name="varun" />
+//         </Fragment>
+//     )
+// }
+
+// class App extends Component {
+//     render() {
+//         return (
+//             <Fragment>
+//                 <Student name="Rahul" />
+//                 <Student name="Ram" />
+//             </Fragment>
+//         )
+//     }
+// }
+
+// export default App;
---
+++ src/Basics of React js/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+// import el from "./App";
+// import Student from "./App";
+// import App from "./App";
+// import el from "./App";
+
+// import App from "./App";
+import Student from "./Student";
+
+// let el = < h1 > Hello World! < /h1>   
+/* react element is created, it is same as 'React.createElement("h1", null, "Hello World!")' */
+
+// ReactDom.render(el, document.getElementById("root"));
+
+// ReactDom.render(< App />, document.getElementById("root"));
+
+// ReactDom.render(el, document.getElementById("root"));
+// ReactDom.render(<Student name="Sayuj" />, document.getElementById("root"));
+
+// ReactDom.render(<App />, document.getElementById("root"));
+
+ReactDom.render(<Student name="sayuj" roll="101" />, document.getElementById("root"));
+
